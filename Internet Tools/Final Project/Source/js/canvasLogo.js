a80f8b2d2768cc61b8791134aabfcee49eaf8405
Tyler Wilding <xtvaser@gmail.com>
2016-11-08 15:37:35 -0500

    Finished Projects Page

---
+++ Internet Tools/Final Project/Source/js/canvasLogo.js
@@ -0,0 +1,42 @@
+var context = document.getElementById("canvasLogo").getContext("2d");
+setInterval("animate()", 1);
+var text = "Tyler Wilding";
+var textDirection = "right";
+var textXpos = 5;
+
+
+
+function init() {
+
+
+}
+
+function animate() {
+    // Clear screen
+    context.clearRect(0, 0, 500, 500);
+    context.globalAlpha = 1;
+    context.fillStyle = '#fff';
+    context.fillRect(0, 0, 500, 500);
+
+    var metrics = context.measureText(text);
+    var textWidth = metrics.width;
+
+    if (textDirection == "right") {
+        textXpos += 10;
+
+        if (textXpos > 350 - textWidth) {
+            textDirection = "left";
+        }
+    } else {
+        textXpos -= 10;
+
+        if (textXpos < 10) {
+            textDirection = "right";
+        }
+    }
+
+    context.font = '20px _sans';
+    context.fillStyle = '#FF0000';
+    context.textBaseline = 'top';
+    context.fillText(text, textXpos, 10);
+}
