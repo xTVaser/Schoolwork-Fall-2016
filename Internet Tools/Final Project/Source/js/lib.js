c74286a0eb53892fe16d3b14ef1127241833ad41
Tyler Wilding <xtvaser@gmail.com>
2016-11-12 14:19:04 -0500

    Added Shape Requirement

--- Internet Tools/Final Project/Source/js/lib.js
+++ Internet Tools/Final Project/Source/js/lib.js
@@ -38,7 +38,7 @@ function randomInt(min, max) {
 
 //Returns a random int in a range, inclusive.
 function randomDouble(min, max) {
-        return Math.random() * (max - min + 1) + min;
+        return Math.random() * (max - min) + min;
 }
 
 //Converts degrees to radians.
@@ -49,4 +49,19 @@ function toRads(degrees) {
 //get the magnitude of a 3D vector
 function magnitude(vector3) {
         return vector3.x * vector3.x + vector3.y * vector3.y + vector3.z * vector3.z;
+}
+
+function randomMatrixTransform() {
+
+        var scaleX = randomDouble(0, 1);
+        var scaleY = randomDouble(0, 1);
+        var skewX = randomDouble(0, 0.15);
+        var skewY = randomDouble(0, 0.15);
+
+        return "matrix(" + scaleX + ", " + skewY + ", " + skewX + ", " + scaleY + ", 0, 0)";
+}
+
+function randomColor() {
+
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
---
+++ Internet Tools/Final Project/Source/js/shapes.js
@@ -0,0 +1,15 @@
+var shapeContainer = document.getElementById("shapeGallery");
+
+function newShape() {
+
+    shapeContainer.style.backgroundColor = randomColor();
+    shapeContainer.style.borderRadius = randomInt(1, 100) + "%";
+    shapeContainer.style.transform = randomMatrixTransform();
+    shapeContainer.style.borderLeft = randomInt(1, 100) + "px solid " + Math.random() > 0.5 ? "transparent" : randomColor();
+    shapeContainer.style.borderRight = randomInt(1, 100) + "px solid " + Math.random() > 0.5 ? "transparent" : randomColor();
+    shapeContainer.style.borderTop = randomInt(1, 100) + "px solid " + Math.random() > 0.5 ? "transparent" : randomColor();
+    shapeContainer.style.borderBottom = randomInt(1, 100) + "px solid " + Math.random() > 0.5 ? "transparent" : randomColor();
+    shapeContainer.style.transition = randomDouble(0.25, 2.5) + "s";
+}
+
+setInterval(newShape, 2500);
