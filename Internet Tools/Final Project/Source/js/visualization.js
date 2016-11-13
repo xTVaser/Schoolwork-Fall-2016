8e338f84841fb90dd86702483c26847883b9e60f
Tyler Wilding <xtvaser@gmail.com>
2016-11-13 15:41:47 -0500

    Final canvas idea is getting there

--- Internet Tools/Final Project/Source/js/visualization.js
+++ Internet Tools/Final Project/Source/js/visualization.js
@@ -1,35 +1,68 @@
-//ctx.fillRect(0, 0, 10, volume);
+var menu = document.getElementById("songMenu");
+var audio = document.getElementById("visualizationSong");
+var slider = document.getElementById("volSlider");
 
-var canvas = document.getElementById('canvasNonLogo');
-fitToContainer(canvas);
+audio.src = "../assets/sounds/song1.mp3"; //Default song to play
+audio.controls = false;
+audio.autoplay = true;
 
-function fitToContainer(canvas) {
-    // Make it visually fill the positioned parent
-    canvas.style.width = '75%';
-    canvas.style.height = '350px';
-    // ...then set the internal size to match
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+var api = new AudioContext() || new webkitAudioContext();
+var anal = api.createAnalyser();
+
+var visualContainer = document.getElementById("visualizationContainer");
+var visualCanvas = document.getElementById("visualization");
+
+visualCanvas.style.width = visualContainer.width/2;
+visualCanvas.style.height = 300;
+visualCanvas.width = visualContainer.offsetWidth/2;
+visualCanvas.height = 300;
+
+var visual = visualCanvas.getContext("2d");
+var lastFrame;
+
+
+function beginVisualization() {
+
+    var init = api.createMediaElementSource(audio);
+    init.connect(anal);
+    anal.connect(api.destination);
+
+    animate();
 }
 
-var theCanvas = canvas;
-var theContext = theCanvas.getContext("2d");
+function animate() {
 
-function drawScreen() {
-    //background
-    theContext.fillStyle = "#ffffaa";
-    theContext.fillRect(0, 0, 500, 300);
+    streamData = new Uint8Array(128); //128 bins of frequency data
+    anal.getByteFrequencyData(streamData);
 
-    //text
-    theContext.fillStyle = "#000000";
-    theContext.font = "20px _sans";
-    theContext.textBaseline = "top";
-    theContext.fillText("IM A CANVAS!", 195, 80);
+    for(bin = 0; bin < streamData.length; bin++) {
 
-    //box
-    theContext.strokeStyle = "#000000";
-    theContext.strokeRect(5, 5, 490, 290);
+        var val = streamData[bin];
+        var red = val;
+        var green = 255 - val;
+        var blue = val / 2;
+        visual.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+        visual.fillRect(bin * 2, 0, 2, visual.height);
+    }
 
+    requestAnimationFrame(animate);
 }
 
-drawScreen();
+menu.addEventListener("input", function(e) {
+
+    if(e.target.selectedOptions[0].value === "Song 1")
+        audio.src = "../assets/sounds/song1.mp3";
+    else if(e.target.selectedOptions[0].value === "Song 2")
+        audio.src = "../assets/sounds/song2.mp3";
+    else if(e.target.selectedOptions[0].value === "Song 3")
+        audio.src = "../assets/sounds/song3.mp3";
+
+    audio.play();
+});
+
+slider.addEventListener("change", function(e) {
+
+    audio.volume = e.currentTarget.value/100.0;
+});
+
+beginVisualization();
