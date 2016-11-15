e39477abaf73198bcb93f066082a219bcd8a2f98
Tyler Wilding <xtvaser@gmail.com>
2016-11-14 23:40:17 -0500

    Canvas complete

--- Internet Tools/Final Project/Source/js/visualization.js
+++ Internet Tools/Final Project/Source/js/visualization.js
@@ -1,20 +1,30 @@
 var menu = document.getElementById("songMenu");
 var audio = document.getElementById("visualizationSong");
 var slider = document.getElementById("volSlider");
+var topColorDOM = document.getElementById("topColor");
+var botColorDOM = document.getElementById("botColor");
+
+var topColor = topColorDOM.value;
+var botColor = botColorDOM.value;
 
 audio.src = "../assets/sounds/song1.mp3"; //Default song to play
 audio.controls = false;
 audio.autoplay = true;
+audio.volume = 25/100;
+audio.loop = true;
 
-var api = new AudioContext() || new webkitAudioContext();
+slider.value = 25;
+menu.selectedIndex = 0;
+
+var api = new (window.AudioContext || window.webkitAudioContext)();
 var anal = api.createAnalyser();
 
 var visualContainer = document.getElementById("visualizationContainer");
 var visualCanvas = document.getElementById("visualization");
 
-visualCanvas.style.width = visualContainer.width/2;
+visualCanvas.style.width = visualContainer.width*0.75;
 visualCanvas.style.height = 300;
-visualCanvas.width = visualContainer.offsetWidth/2;
+visualCanvas.width = visualContainer.offsetWidth*0.75;
 visualCanvas.height = 300;
 
 var visual = visualCanvas.getContext("2d");
@@ -29,20 +39,33 @@ function beginVisualization() {
 
     animate();
 }
+var barWidth = visual.canvas.width/128/2;
+var barPadding = barWidth;
+var barHeight = visual.canvas.height;
 
 function animate() {
+
+    visual.clearRect(0, 0, visual.canvas.width, visual.canvas.height);
 
     streamData = new Uint8Array(128); //128 bins of frequency data
     anal.getByteFrequencyData(streamData);
 
     for(bin = 0; bin < streamData.length; bin++) {
 
-        var val = streamData[bin];
+        //Color of Bar
+        var val = streamData[bin];  //From 1 to 255 (max size of a byte)
         var red = val;
         var green = 255 - val;
         var blue = val / 2;
-        visual.fillStyle = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
-        visual.fillRect(bin * 2, 0, 2, visual.height);
+
+        var gradient = visual.createLinearGradient(visual.canvas.width/2, visual.canvas.height, visual.canvas.width/2, 0);
+        gradient.addColorStop("0", botColor);
+        gradient.addColorStop("1.0", topColor);
+
+        //Creating Bar
+        var barX = (bin * barWidth)+(bin * barPadding);
+        visual.fillStyle = gradient;//'rgb(' + red + ', ' + green + ', ' + blue + ')';
+        visual.fillRect(barX, barHeight, -barWidth, -barHeight*(val/255));
     }
 
     requestAnimationFrame(animate);
@@ -60,9 +83,31 @@ menu.addEventListener("input", function(e) {
     audio.play();
 });
 
+menu.addEventListener("change", function(e) {
+
+    if(e.target.selectedOptions[0].value === "Song 1")
+        audio.src = "../assets/sounds/song1.mp3";
+    else if(e.target.selectedOptions[0].value === "Song 2")
+        audio.src = "../assets/sounds/song2.mp3";
+    else if(e.target.selectedOptions[0].value === "Song 3")
+        audio.src = "../assets/sounds/song3.mp3";
+
+    audio.play();
+});
+
 slider.addEventListener("change", function(e) {
 
     audio.volume = e.currentTarget.value/100.0;
+});
+
+topColorDOM.addEventListener("change", function(e) {
+
+    topColor = topColorDOM.value;
+});
+
+botColorDOM.addEventListener("change", function(e) {
+
+    botColor = botColorDOM.value;
 });
 
 beginVisualization();
